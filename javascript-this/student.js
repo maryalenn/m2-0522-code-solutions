4dbddbe4b7b7e13bc0c6bf978cbf9f0b1ff17971
Marya Chuong <maryachuong@gmail.com>
2022-05-10 04:08:47 +0000

    Completed JavaScript This exercise

---
+++ javascript-this/student.js
@@ -0,0 +1,8 @@
+/* exported student */
+var student = {
+  firstName: 'Marya',
+  lastName: 'Chuong',
+  subject: 'JavaScript',
+  getFullName: function () { return this.firstName + ' ' + this.lastName; },
+  getIntroduction: function () { return 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.'; }
+};
